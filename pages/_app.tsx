dfdbfba6745c680f5dedec44bf3971cdfac4fd01
sho kawamoto <k.sho626626@gmail.com>
2026-01-26 07:32:57 +0900

    fix: Google Fontsのfetch失敗対策にdisplay: swapを追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import { Inter } from 'next/font/google';
 const inter = Inter({
   variable: '--font-inter',
   subsets: ['latin'],
+  display: 'swap',
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
